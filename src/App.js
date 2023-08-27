ed184a7d6c014e74553b2313e1d892d2eecf350a
მაცი <93665709+maci03@users.noreply.github.com>
2023-08-27 14:44:47 +0400

    added wine shope code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
+import React from 'react';
+
+const wine = [
+  {id: 1, name: "mukuzani", price: 45},
+  {id: 2, name: "saperavi", price: 55},
+  {id: 3, name: "kisi", price: 42}
+]
+
+
+
+
 
 function App() {
   return (
     <div className="App">
-    
+      <h1>Wine Shop</h1>
+        <div className='wine-list'>
+          <h2>Wine List</h2>
+          <ul>
+            {wines.map(wine =>(
+              <li key={wine.id}>
+                {wine.name} - {wine.price}
+              </li>
+            ))}
+          </ul>
+        </div>
     </div>
   );
 }
