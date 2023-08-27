7fe702468dde66c0b314fd4429c4fce99fa8ede2
მაცი <93665709+maci03@users.noreply.github.com>
2023-08-27 14:59:51 +0400

    added cart.js page

--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
-import './App.css';
 import React from 'react';
+import './App.css';
 
-const wine = [
-  {id: 1, name: "mukuzani", price: 45},
-  {id: 2, name: "saperavi", price: 55},
-  {id: 3, name: "kisi", price: 42}
-]
-
-
-
-
+const wines = [
+  { id: 1, name: 'Cabernet Sauvignon', price: 25.99 },
+  { id: 2, name: 'Chardonnay', price: 18.99 },
+  { id: 3, name: 'Merlot', price: 20.99 },
+];
 
 function App() {
   return (
     <div className="App">
-      <h1>Wine Shop</h1>
-        <div className='wine-list'>
-          <h2>Wine List</h2>
-          <ul>
-            {wines.map(wine =>(
-              <li key={wine.id}>
-                {wine.name} - {wine.price}
-              </li>
-            ))}
-          </ul>
-        </div>
+      <h1>Wine Store</h1>
+      <div className="wine-list">
+        <h2>Wine List</h2>
+        <ul>
+          {wines.map(wine => (
+            <li key={wine.id}>
+              {wine.name} - ${wine.price}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
