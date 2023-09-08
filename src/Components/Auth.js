f965c828f2be3b1542e3d6a97b63261109c4acd7
მაცი <93665709+maci03@users.noreply.github.com>
2023-09-08 12:02:01 +0400

    firebase file

--- src/Components/Auth.js
+++ src/Components/Auth.js
@@ -1,34 +1,60 @@
-import React, { useState } from "react";
-import firebase from "./firebase";
 
-function Auth(){
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [user, setUser] = useState(null);
+import React, { useState } from "react";
+import firebase from "../Components/firebase";
+
+function Auth() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
+
+  const handleSignUp = async () => {
+    try {
+      const userCredential = await firebase.auth().createUserWithEmailAndPassword(email, password);
+      setUser(userCredential.user);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  const handleSignIn = async () => {
+    try {
+      const userCredential = await firebase.auth().signInWithEmailAndPassword(email, password);
+      setUser(userCredential.user);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  const handleSignOut = () => {
+    firebase.auth().signOut();
+    setUser(null);
+  };
+
+  return (
+    <div>
+      {user ? (
+        <div>
+          <p>Welcome, {user.email}!</p>
+          <button onClick={handleSignOut}>Sign Out</button>
+        </div>
+      ) : (
+        <div>
+          <input
+            type="text"
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button onClick={handleSignIn}>Sign In</button>
+          <button onClick={handleSignUp}>Sign Up</button>
+        </div>
+      )}
+    </div>
+  );
 }
 
-const handleSingUp = async () => {
-    try{
-        const userCredential = await firebase.auth().createUserWithEmailAndPassword(email, password);
-        setUser(userCredential.user);
-    }catch(error){
-        console.error(error.massage);
-    }
-};
-const handleSingOut = () =>{
-    firebase.auth().signOut();
-    setUser(null)
-};
-
-return(
-    <div>
-    {user ? (
-      <div>
-        <p>Welcome, {user.email}!</p>
-        <button onClick={handleSignOut}>Sign Out</button>
-      </div>
-    ) : (
-        <div>
-            <input type="text" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
-            
-        </div>
+export default Auth;
